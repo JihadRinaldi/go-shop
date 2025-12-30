b7f56ac8c069090b9fa209f40fc3a2e29110a33a
JihadRinaldi <jihadmjr@gmail.com>
2025-12-30 10:34:08 +0700

    refactor: use repository pattern instead of direct query on service

---
+++ internal/repositories/cart_repository.go
@@ -0,0 +1,31 @@
+package repositories
+
+import (
+	"github.com/JihadRinaldi/go-shop/internal/models"
+	"gorm.io/gorm"
+)
+
+type CartRepository struct {
+	db *gorm.DB
+}
+
+func NewCartRepository(db *gorm.DB) *CartRepository {
+	return &CartRepository{db: db}
+}
+
+func (r *CartRepository) GetByUserID(userID uint) (*models.Cart, error) {
+	var cart models.Cart
+	if err := r.db.Where("user_id = ?", userID).First(&cart).Error; err != nil {
+		return nil, err
+	}
+	return &cart, nil
+}
+func (r *CartRepository) Create(cart *models.Cart) error {
+	return r.db.Create(cart).Error
+}
+func (r *CartRepository) Update(cart *models.Cart) error {
+	return r.db.Save(cart).Error
+}
+func (r *CartRepository) Delete(id uint) error {
+	return r.db.Delete(&models.Cart{}, id).Error
+}
